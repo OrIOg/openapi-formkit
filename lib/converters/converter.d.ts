ca78543cdef42c467974ac32755a4b46863d765f
Thibault Poisson <21706220@etu.unicaen.fr>
2022-07-12 13:59:57 +0200

    build

--- lib/converters/converter.d.ts
+++ lib/converters/converter.d.ts
@@ -2,11 +2,20 @@ import { ParameterObject, RequestBodyObject, OpenAPIObject } from 'openapi3-ts';
 import { FormKitGroup, FormKitItem, Parameter, Options, method, Route } from '../types';
 export default class Converter {
     protected options: Options;
-    constructor(options: Options);
+    constructor(options: Partial<Options>);
+    static OperationToForm(path: string, op: method, options: Options, parameters: FormKitItem[]): {
+        $cmp: string;
+        props: {
+            type: string;
+            method: method;
+        };
+        children: FormKitItem[];
+    };
     convert(obj: OpenAPIObject): Record<string, Record<method, Route>>;
     readParameter(param: Parameter): FormKitItem | undefined;
     readObject(parameter: Parameter): FormKitGroup;
-    applyTransformers(parameter: Parameter, item: FormKitItem): void;
+    applyTransformers(parameter: Parameter, item: FormKitItem): FormKitItem;
+    applyOperationTransformers(path: string, op: method, item: Route): Route;
     readParameters(parameters: ParameterObject[]): FormKitItem[];
     readRequestBody(body: RequestBodyObject): FormKitItem[];
 }
